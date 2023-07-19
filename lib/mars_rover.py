6e9acfab9143d78209063e8990d332b33e6d8cdc
Becka <rebecca.lelew@gmail.com>
2023-07-19 21:42:43 +0100

    Add mars rover fp

--- lib/mars_rover.py
+++ lib/mars_rover.py
@@ -1,6 +1,7 @@
 from lib.models import MarsRover, Coord, Grid, update_mars_rover_coord, update_mars_rover_direction
 from typing import List
 
+
 def move_one(mars_rover: MarsRover, reverse: bool, grid: Grid) -> MarsRover:
     directions_to_deltas = {"N": (0, 1), "S": (0, -1), "E": (1, 0), "W": (-1, 0)}
     (x_delta, y_delta) = directions_to_deltas[mars_rover.direction]
@@ -9,25 +10,27 @@ def move_one(mars_rover: MarsRover, reverse: bool, grid: Grid) -> MarsRover:
     else:
         return update_mars_rover_coord(mars_rover, x_delta, y_delta, grid)
 
+
 def turn(mars_rover: MarsRover, clockwise: bool) -> MarsRover:
     directions_list = ["N", "E", "S", "W"]
     current_direction_index = directions_list.index(mars_rover.direction)
-    next = current_direction_index + 1 if clockwise else current_direction_index - 1
-    return update_mars_rover_direction(mars_rover, directions_list[next % len(directions_list)])
+    next_index = current_direction_index + 1 if clockwise else current_direction_index - 1
+    return update_mars_rover_direction(mars_rover, directions_list[next_index % len(directions_list)])
+
 
 def run(commands: List[str], rover: MarsRover, grid: Grid, obstacles: List[Coord]) -> tuple[MarsRover, bool]:
     if len(commands) == 0:
-        return (rover, True)
+        return rover, True
     else:
         first_command, *remaining_commands = commands
         transformation_function = {
-            "l": lambda rover: turn(rover, False), 
-            "r": lambda rover: turn(rover, True), 
+            "l": lambda rover: turn(rover, False),
+            "r": lambda rover: turn(rover, True),
             "f": lambda rover: move_one(rover, False, grid),
             "b": lambda rover: move_one(rover, True, grid)
         }[first_command]
         new_rover = transformation_function(rover)
         if new_rover.coord in obstacles:
-            return (rover, False)
+            return rover, False
         else:
             return run(remaining_commands, new_rover, grid, obstacles)
--- lib/models.py
+++ lib/models.py
@@ -1,30 +1,33 @@
 from dataclasses import dataclass, replace
 from enum import Enum
 
-@dataclass
+
+@dataclass(frozen=True)
 class Coord:
     x: int
     y: int
-    
+
+
 Direction = Enum('Direction', ['N', 'S', 'E', 'W'])
 
-@dataclass
+
+@dataclass(frozen=True)
 class MarsRover:
     coord: Coord
     direction: Direction
 
-@dataclass
+
+@dataclass(frozen=True)
 class Grid:
     x_max: int
     y_max: int
 
+
 def update_mars_rover_coord(mars_rover: MarsRover, x_by: int, y_by: int, grid: Grid) -> MarsRover:
     new_y = (mars_rover.coord.y + y_by) % (grid.y_max + 1)
-    new_x =(mars_rover.coord.x + x_by) % (grid.x_max + 1)
-    return replace(mars_rover, coord = replace(mars_rover.coord, x=new_x, y=new_y))
+    new_x = (mars_rover.coord.x + x_by) % (grid.x_max + 1)
+    return replace(mars_rover, coord=replace(mars_rover.coord, x=new_x, y=new_y))
+
 
 def update_mars_rover_direction(mars_rover: MarsRover, new_direction: Direction) -> MarsRover:
-    return replace(mars_rover, direction = new_direction)
-
-        
-
+    return replace(mars_rover, direction=new_direction)
